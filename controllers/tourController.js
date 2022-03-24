1cced1c70a17880364e951c9b14fd30258124711
yonaSisay <yonimelkamu357@gmail.com>
2022-03-25 01:46:03 +0300

    Adding addtional filtering methods to the Api and collect the to new module called apiFeatures

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,35 +1,25 @@
 const Tour = require('./../models/tourModel')
+const APIFeatures = require('./../utils/apiFeatures')
+
+exports.aliasTopTours= (req, res, next)=> {
+    
+    req.query.limit = '5';
+    req.query.sort = '-ratingsAverage,price';
+    req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+    next();
+};
 
 
 exports.getAllTours = async (req, res)=>{
     
     try {
-        // BUILD QUERY
-        //1A Filtering 
-        const queryObj = {...req.query};
-        const excludedFields = ['page', 'sort', 'limits', 'fields'];
-        excludedFields.forEach(el=> delete queryObj[el]);
-        
-        
-        //1B Advanced filtering
-        let queryStr = JSON.stringify(queryObj);
-        
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-        console.log(JSON.parse(queryStr));
-        
-        let query =  Tour.find(JSON.parse(queryStr));
-
-        // 2)Sorting
-        if(req.query.sort) {
-            const sortBy = req.query.sort.split(',').join(' ');
-            console.log(sortBy)
-            query = query.sort(sortBy)
-        }else{
-            query = query.sort('-createdAt');
-        }
-
         // EXECUTE QUERY
-        const tours = await query;
+        const features = new APIFeatures(Tour.find(), req.query)
+        .filter()
+        .sort()
+        .paginate()
+        .limitFields();
+        const tours = await features.query;
 
     //SEND RESPONSE
     res.status(200).json({
