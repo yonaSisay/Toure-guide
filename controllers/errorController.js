9f8354ae7062d69953782ecead3c8580377be2d3
yonaSisay <yonimelkamu357@gmail.com>
2022-04-09 13:10:25 +0300

    Adding Validation Error handler to the project

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -10,6 +10,12 @@ const handleDuplicateDB = err =>{
     return new AppError(message, 400);
 }
 
+const handleValidationErrorDB = err =>{
+    const errors = Object.values(err.errors).map(el => el.message)
+    const message = `Invalid input data. ${errors.join('. ')}`;
+    return new AppError(message, 400);
+}
+
 const sendErrorDev = (err, res)=>{
     res.status(err.statusCode).json({
         status: err.status,
@@ -45,6 +51,7 @@ module.exports = (err, req, res, next)=>{
         let error = { ...err };
         if(err.name === 'CastError' ) err  = handleCastErrorDB(err);
         if(err.code === 11000) err = handleDuplicateDB(err);
+        if(err.name === 'ValidationError') err = handleValidationErrorDB(err)
 
        sendErrorProd(err, res);
     }
